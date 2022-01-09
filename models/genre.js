b1b20fb1b9c753abbaab0d3ed88850f1d386284b
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-09 13:21:44 -0600

    created new genre model

---
+++ models/genre.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: { type: string, minlength: 3, maxlength: 100 },
+});
+
+AuthorSchema.virtual('url').get(function () {
+  return `/catalog/genre/${this._id}`;
+});
