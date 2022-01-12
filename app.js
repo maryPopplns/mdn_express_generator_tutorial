195370fbc52d920ae615d4af0a91902fd17b7bec
Spencer Knight <spencer.knightusn@gmail.com>
2022-01-12 09:26:49 -0700

    added wiki route

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require(path.join(__dirname, 'routes/index'));
+var usersRouter = require(path.join(__dirname, 'routes/users'));
+var wikiRouter = require(path.join(__dirname, 'routes/wiki'));
 
 var app = express();
 
